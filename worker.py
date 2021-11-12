1efe55071d2a9cdda2b5b878506d66065f73c5d1
rdedo099 <43250257+rdedo099@users.noreply.github.com>
2021-11-12 03:18:18 -0500

    Updated - ran trigram+bigram
    To do:
    -need to sort out re-clustering when node is not agreed upon
    -need to iron out when node is agreed upon but gets assigned incorrectly

--- worker.py
+++ worker.py
@@ -22,6 +22,17 @@ def nearest(x, y_row, neighbors, labels): #specify number of neighbours to retur
         neighborsList.append(distances[i][0])
         neighborsLabels.append(distances[i][1:len(distances)-1])
     return neighborsList,neighborsLabels
+    
+def lookUp(labels, lookup, which):
+    arr = np.array(labels)
+    x,y = np.shape(arr)
+    indices = np.argwhere(arr[which] == lookup)
+    return indices
+def firstTrue(boolArr):
+    for i in range(len(boolArr)):
+        if boolArr[i] == True:
+            return i
+
 
 def isEqual(x,y):
     if x == y:
@@ -34,24 +45,37 @@ def checkClus(x,y):
     boolArr = list(map(isEqual, x, y))
     return boolArr
 
-def consensus(boolArr): #form boolean array from checkClus, if majority are true then the node should be in the cluster, otherwise not
-    trueCount, total = sum(boolArr), len(boolArr)
+def consensus(x,y,labels): #form boolean array from checkClus, if majority are true then the node should be in the cluster, otherwise not
+    boolArr=checkClus(x,y)
+    trueCount, total,first = sum(boolArr), len(boolArr), firstTrue(boolArr)
     if (trueCount)/(total) > 0.5:
         #pass vote of node being in cluster
-        return True
+        if (boolArr[0] == False):
+            print("did not fully pass")
+            print(x)
+            print(y)
+            ind1 = lookUp(labels,x[first],first)
+            ind2 = lookUp(labels,x[0],0)
+            finalind = next((a for a in ind1 if a in ind2), None)
+            xVal = labels[0][finalind]
+            return True,xVal
+        return True, x[0]
     else:
+        print("failed!")
         #pass the false vote - node is not in cluster
-        return False
+        return False,0#,recluster(x,y,labels)
 
-def comprehension(X,labels,x):
-    print(x)
+def comprehension(X,labels,x,itera):
+    if itera > 0:
+        X = np.delete(X,[0,itera-1], axis = 0)
+        labels = np.delete(labels,[0,itera-1], axis = 1)
     near, tempLab = nearest(X, x, 3, labels)
-    boolArr1 = checkClus(tempLab[0],tempLab[1])
-    boolArr2 = checkClus(tempLab[0],tempLab[2])
-    if consensus(boolArr1):
-        return tempLab[0][0] #need to sort this out such that we assign a valid cluster number
-    elif consensus(boolArr2): #here we check its second nearest neighbor before reclustering
-        return tempLab[1][0]
+    boolArr1,res1 = consensus(tempLab[0],tempLab[1],labels)
+    boolArr2,res2 = consensus(tempLab[0],tempLab[2],labels)
+    if boolArr1:
+        return res1 #need to sort this out such that we assign a valid cluster number
+    elif boolArr2: #here we check its second nearest neighbor before reclustering
+        return res2
     else: #recluster somehow
         return tempLab[0][0]
 
